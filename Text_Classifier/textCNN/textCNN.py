3facd5c850315516c3e071c09e7f8540f159e30a
Biosciman <46929046+Biosciman@users.noreply.github.com>
2022-12-08 11:44:42 +0800

    Add files via upload

--- Text_Classifier/textCNN/textCNN.py
+++ Text_Classifier/textCNN/textCNN.py
@@ -1,3 +1,5 @@
+# 参考https://www.cnblogs.com/jiangxinyang/p/10207482.html
+
 import os
 import csv
 import time
@@ -13,4 +15,218 @@ import pandas as pd
 import numpy as np
 import tensorflow as tf
 from sklearn.metrics import roc_auc_score, accuracy_score, precision_score, recall_score
+
+# 配置参数
+class TrainingConfig(object):
+    epoches = 5
+    evaluateEvery = 100
+    checkpointEvery = 100
+    learningRate = 0.001
+
+class ModelConfig(object):
+    embeddingSize = 200
+    numFilters = 128
+    filterSizes = [2, 3, 4, 5]
+    dropoutKeepProb = 0.5
+    l2RegLambda = 0.0
+
+class Config(object):
+    sequenceLength = 200  # 取了所有序列长度的均值
+    batchSize = 128
+    dataSource = "labeledTrain.csv"
+    stopWordSource = "english.txt"
+    numClasses = 1  # 二分类设置为1，多分类设置为类别的数目
+    rate = 0.8  # 训练集的比例
+    training = TrainingConfig()
+    model = ModelConfig()
+
+# 实例化配置参数对象
+config = Config()
+
+"""
+生成训练数据
+1）将数据加载进来，将句子分割成词表示，并去除低频词和停用词。
+2）将词映射成索引表示，构建词汇-索引映射表，并保存成json的数据格式，之后做inference时可以用到。
+（注意，有的词可能不在word2vec的预训练词向量中，这种词直接用UNK表示）
+3）从预训练的词向量模型中读取出词向量，作为初始化值输入到模型中。
+4）将数据集分割成训练集和测试集
+"""
+
+# 数据预处理的类，生成训练集和测试集
+class Dataset(object):
+    def __init__(self, config):
+        self.config = config
+        self._dataSource = config.dataSource
+        self._stopWordSource = config.stopWordSource
+
+        self._sequenceLength = config.sequenceLength  # 每条输入的序列处理为定长
+        self._embeddingSize = config.model.embeddingSize
+        self._batchSize = config.batchSize
+        self._rate = config.rate
+
+        self._stopWordDict = {}
+
+        self.trainReviews = []
+        self.trainLabels = []
+
+        self.evalReviews = []
+        self.evalLabels = []
+
+        self.wordEmbedding = None
+
+        self.labelList = []
+
+    def _readData(self, filePath):
+        """
+        从csv文件中读取数据集
+        """
+
+        df = pd.read_csv(filePath)
+
+        if self.config.numClasses == 1:
+            labels = df["sentiment"].tolist()
+        elif self.config.numClasses > 1:
+            labels = df["rate"].tolist()
+
+        review = df["review"].tolist()
+        reviews = [line.strip().split() for line in review]
+
+        return reviews, labels
+
+    def _genVocabulary(self, reviews, labels):
+        """
+        生成词向量和词汇-索引映射字典，可以用全数据集
+        """
+
+        allWords = [word for review in reviews for word in review]
+
+        # 去掉停用词
+        subWords = [word for word in allWords if word not in self.stopWordDict]
+
+        wordCount = Counter(subWords)  # 统计词频,Counter返回类dict
+        sortWordCount = sorted(wordCount.items(), key=lambda x: x[1], reverse=True)
+
+        # 去除低频词
+        words = [item[0] for item in sortWordCount if item[1] >= 5]
+
+        vocab, wordEmbedding = self._getWordEmbedding(words)
+        self.wordEmbedding = wordEmbedding
+
+        word2idx = dict(zip(vocab, list(range(len(vocab)))))
+
+        uniqueLabel = list(set(labels))  # 确定分多少类
+
+        label2idx = dict(zip(uniqueLabel, list(range(len(uniqueLabel)))))  # zip返回元组
+        self.labelList = list(range(len(uniqueLabel)))
+
+        # 将词汇-索引映射表保存为json数据，之后做inference时直接加载来处理数据
+        with open("word2idx.json", "w", encoding="utf-8") as f:
+            json.dump(word2idx, f)
+
+        with open("label2idx.json", "w", encoding="utf-8") as f:
+            json.dump(label2idx, f)
+
+        return word2idx, label2idx
+
+    def _labelToIndex(self, labels, label2idx):
+        """
+        将标签转换成索引表示
+        """
+        labelIds = [label2idx[label] for label in labels]
+        return labelIds
+
+    def _wordToIndex(self, reviews, word2idx):
+        """
+        将词转换成索引
+        """
+        reviewIds = [[word2idx.get(item, word2idx["UNK"]) for item in review] for review in reviews]
+        return reviewIds
+
+    def _genTrainEvalData(self, x, y, word2idx, rate):
+        """
+        生成训练集和验证集
+        """
+        reviews = []
+        for review in x:
+            if len(review) >= self._sequenceLength:
+                reviews.append(review[:self._sequenceLength])
+            else:
+                reviews.append(review + [word2idx["PAD"]] * (self._sequenceLength - len(review)))
+
+        trainIndex = int(len(x) * rate)
+
+        trainReviews = np.asarray(reviews[:trainIndex], dtype="int64")
+        trainLabels = np.array(y[:trainIndex], dtype="float32")
+
+        evalReviews = np.asarray(reviews[trainIndex:], dtype="int64")
+        evalLabels = np.array(y[trainIndex:], dtype="float32")
+
+        return trainReviews, trainLabels, evalReviews, evalLabels
+
+    def _getWordEmbedding(self, words):
+        """
+        按照我们的数据集中的单词取出预训练好的word2vec中的词向量
+        """
+
+        wordVec = gensim.models.KeyedVectors.load_word2vec_format("../word2vec/word2Vec.bin", binary=True)
+        vocab = []
+        wordEmbedding = []
+
+        # 添加 "pad" 和 "UNK",
+        vocab.append("PAD")
+        vocab.append("UNK")
+        wordEmbedding.append(np.zeros(self._embeddingSize))
+        wordEmbedding.append(np.random.randn(self._embeddingSize))
+
+        for word in words:
+            try:
+                vocab.append(word)
+                vector = wordVec.wv[word]
+                wordEmbedding.append(vector)
+            except:
+                None
+                # print(word + "不存在于词向量中")
+        return vocab, np.array(wordEmbedding)
+
+    def _readStopWord(self, stopWordPath):
+        """
+        读取停用词
+        """
+
+        with open(stopWordPath, "r") as f:
+            stopWords = f.read()
+            stopWordList = stopWords.splitlines()
+            # 将停用词用列表的形式生成，之后查找停用词时会比较快
+            self.stopWordDict = dict(zip(stopWordList, list(range(len(stopWordList)))))
+
+    def dataGen(self):
+        """
+        初始化训练集和验证集
+        """
+
+        # 初始化停用词
+        self._readStopWord(self._stopWordSource)
+
+        # 初始化数据集
+        reviews, labels = self._readData(self._dataSource)
+
+        # 初始化词汇-索引映射表和词向量矩阵
+        word2idx, label2idx = self._genVocabulary(reviews, labels)
+
+        # 将标签和句子数值化
+        labelIds = self._labelToIndex(labels, label2idx)
+        reviewIds = self._wordToIndex(reviews, word2idx)
+
+        # 初始化训练集和测试集
+        trainReviews, trainLabels, evalReviews, evalLabels = self._genTrainEvalData(reviewIds, labelIds, word2idx,
+                                                                                    self._rate)
+        self.trainReviews = trainReviews
+        self.trainLabels = trainLabels
+
+        self.evalReviews = evalReviews
+        self.evalLabels = evalLabels
+
+
+data = Dataset(config)
+data.dataGen()
 
