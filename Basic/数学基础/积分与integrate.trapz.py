88073a824d238139a6be39b4400c1aac57313300
Biosciman <46929046+Biosciman@users.noreply.github.com>
2023-02-07 12:16:06 +0800

    Add files via upload

---
+++ Basic/数学基础/积分与integrate.trapz.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import scipy.stats as st
+from scipy.integrate import trapz
+import numpy as np
+
+x = np.linspace(-3, 3, 100)
+y = st.norm.pdf(x)  # 正态分布
+plt.plot(x, y)
+
+xs = np.linspace(-0.8, -0.6, 100)
+p = trapz(st.norm.pdf(xs), xs)  # trapz计算积分
+print((f"落在-0.8与——0.6之间的概率是{100*p:4.2f}%"))
+plt.fill_between(xs, st.norm.pdf(xs), color='green')
+
+plt.grid()
+plt.show()
